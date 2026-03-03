044440a6071d67edd094ea4110849536144bf1f7
Matiou123 <mathieulemire02@gmail.com>
2026-03-03 05:13:22 -0500

    Formatté avec une fonction

--- helper.py
+++ helper.py
@@ -24,38 +24,43 @@ def masse_totale(formule: str) -> float:
 
     return masse_totale
 
+def calcule_density_df(chemin_in:str, chemin_out:str) -> None:
+    """Rajoute la colonne de densité dans le DataFrame provenant de `chemin_in` et l'enregistre dans `chemin_out` """
+
+    df = pd.read_csv(chemin_in)
+
+    # On ajoute une nouvelle colonne qui est initialiment la masse_totale de la formule
+    df['density'] = df['Formulae'].apply(masse_totale) 
+
+
+    # Calcule de densité provenant de gflownet/proxy/crystals/density.py
+    for i in range(df.shape[0]):
+        row = df.iloc[i]
+        a, b, c, cos_alpha, cos_beta, cos_gamma = (
+            row['a'],
+            row['b'],
+            row['c'],
+            torch.cos(torch.deg2rad(torch.tensor(row['alpha']))),
+            torch.cos(torch.deg2rad(torch.tensor(row['beta']))),
+            torch.cos(torch.deg2rad(torch.tensor(row['gamma']))),
+        )
+        volume = (a * b * c) * torch.sqrt(
+            1
+            - (cos_alpha.pow(2) + cos_beta.pow(2) + cos_gamma.pow(2))
+            + (2 * cos_alpha * cos_beta * cos_gamma)
+        ).item()
+
+        df.at[i, 'density'] = (df.at[i, 'density'] / volume) * DENSITY_CONVERSION
+
+
+    dossier = os.path.dirname(chemin_out)
+    if not os.path.exists(dossier):
+        os.makedirs(dossier, exist_ok=True)
+
+    df.columns.values[0] = '' 
+    df.to_csv(chemin_out, index=False)
+
 chemin_in = "data_/eform/train.csv"
-df = pd.read_csv(chemin_in)
-
-
-df['density'] = df['Formulae'].apply(masse_totale)
-
-
-for i in range(df.shape[0]):
-    row = df.iloc[i]
-    a, b, c, cos_alpha, cos_beta, cos_gamma = (
-        row['a'],
-        row['b'],
-        row['c'],
-        torch.cos(torch.deg2rad(torch.tensor(row['alpha']))),
-        torch.cos(torch.deg2rad(torch.tensor(row['beta']))),
-        torch.cos(torch.deg2rad(torch.tensor(row['gamma']))),
-    )
-    volume = (a * b * c) * torch.sqrt(
-        1
-        - (cos_alpha.pow(2) + cos_beta.pow(2) + cos_gamma.pow(2))
-        + (2 * cos_alpha * cos_beta * cos_gamma)
-    ).item()
-
-    df.at[i, 'density'] = (df.at[i, 'density'] / volume) * DENSITY_CONVERSION
-
 chemin_out = "data_/eform_density/train.csv"
 
-dossier = os.path.dirname(chemin_out)
-
-if not os.path.exists(dossier):
-    os.makedirs(dossier, exist_ok=True)
-
-df.columns.values[0] = ''
-df.to_csv(chemin_out, index=False)
-
+calcule_density_df(chemin_in, chemin_out)
